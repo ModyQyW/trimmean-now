dbe46c4564bc7a785353b3e91e202906315730be
Panpan <wurui.dev@gmail.com>
2024-11-28 14:07:42 +0800

    build: add rsdoctor

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,19 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    rspack(config, { appendPlugins }) {
+      // 仅在 RSDOCTOR 为 true 时注册插件，因为插件会增加构建耗时
+      if (process.env.RSDOCTOR) {
+        appendPlugins(
+          new RsdoctorRspackPlugin({
+            // 插件选项
+          })
+        );
+      }
+    },
+  },
 });
