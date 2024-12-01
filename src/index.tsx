7570b15300bfef2620aec29b05f3134be6ec6d10
Panpan <wurui.dev@gmail.com>
2024-12-01 18:12:39 +0800

    feat: use twMerge

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { PrimeReactProvider } from "primereact/api";
 import Tailwind from "primereact/passthrough/tailwind";
-// import { twMerge } from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 import "./global.css";
 // import "primeicons/primeicons.css";
 
 const primeReactProviderValue = {
   unstyled: true,
   pt: Tailwind,
-  // ptOptions: {
-  //   mergeSections: true,
-  //   mergeProps: true,
-  //   classNameMergeFunction: twMerge,
-  // },
+  ptOptions: {
+    mergeSections: true,
+    mergeProps: true,
+    classNameMergeFunction: twMerge,
+  },
 };
 
 const rootEl = document.getElementById("root");
